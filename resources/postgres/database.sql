41ed756dc456ecadd8c4c952c76e51458f47c0a9
Calista Nguyen <nguyen.calista@gmail.com>
2020-04-17 16:21:31 -0600

    added username col to users
    Added the username col to the users table and also made it so that the database is publically owned.

--- resources/postgres/database.sql
+++ resources/postgres/database.sql
@@ -21,7 +21,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: recipes; Type: TABLE; Schema: public; Owner: calistanguyen
+-- Name: recipes; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.recipes (
@@ -30,24 +30,21 @@ CREATE TABLE public.recipes (
 );
 
 
-ALTER TABLE public.recipes OWNER TO calistanguyen;
-
 --
--- Name: users; Type: TABLE; Schema: public; Owner: calistanguyen
+-- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
     user_id integer NOT NULL,
     firstname character varying(20) NOT NULL,
     lastname character varying(20) NOT NULL,
-    password character varying(20) NOT NULL
+    password character varying(20) NOT NULL,
+    username character varying(20)
 );
 
 
-ALTER TABLE public.users OWNER TO calistanguyen;
-
 --
--- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: calistanguyen
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.users_user_id_seq
@@ -59,24 +56,22 @@ CREATE SEQUENCE public.users_user_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_user_id_seq OWNER TO calistanguyen;
-
 --
--- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: calistanguyen
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
 
 
 --
--- Name: users user_id; Type: DEFAULT; Schema: public; Owner: calistanguyen
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
 
 
 --
--- Data for Name: recipes; Type: TABLE DATA; Schema: public; Owner: calistanguyen
+-- Data for Name: recipes; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 COPY public.recipes (user_id, recipe_ids) FROM stdin;
@@ -84,22 +79,22 @@ COPY public.recipes (user_id, recipe_ids) FROM stdin;
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: calistanguyen
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.users (user_id, firstname, lastname, password) FROM stdin;
+COPY public.users (user_id, firstname, lastname, password, username) FROM stdin;
 \.
 
 
 --
--- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: calistanguyen
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: calistanguyen
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
