1e28fd59e124f88a009cf69c7f89ec867f66e60b
eastonevan <49697008+eastonevan@users.noreply.github.com>
2020-04-19 21:22:36 -0600

    Server Side API calls
    I downloaded a new package that allows really easy server side calls and it just added the files to the node modules folder. The code that I made is using the /test endpoint so if you want to see what it does just run the server file through node and then go to localhost:3000/test

--- server.js
+++ server.js
@@ -11,6 +11,8 @@
   Pg-Promise   - A database tool to help use connect to our PostgreSQL database
 ***********************/
 var express = require('express'); //Ensure our express framework has been added
+//var https = require('https');
+const request = require('request');
 var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
 app.use(bodyParser.json());              // support json encoded bodies
@@ -18,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Create Database Connection
 var pgp = require('pg-promise')();
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -47,6 +50,19 @@ app.get('/recipe', function(req,res){
 app.get('/test',function(req,res){
   //call the API here, save the data
   //res.json({recipe:["broccoli","sandwich"]})
+	//var id = 0;
+	var apiKey='ac9d1996174844fa8bd9d2ba7b497976';
+	var url="https://api.spoonacular.com/recipes/search?apiKey="+apiKey+"&number=1&query=sandwich";
+
+	request( url ,{json:true}, (err,response,body)=>{
+		if(err){return console.log(err);}
+		var id= JSON.parse(body.results[0].id);
+		var new_url='https://api.spoonacular.com/recipes/'+id+'/information?apiKey=ac9d1996174844fa8bd9d2ba7b497976';
+		request(new_url, {json:true}, (err2,response2,body2)=> {
+			res.json(body2);
+		})
+	});
+
 })
 //sign up page
 app.get('/signup', function(req,res){
